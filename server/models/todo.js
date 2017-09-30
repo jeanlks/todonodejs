16f3a7a501e1a4e0bf02c0f4f451399591b69792
bob <jmontecarvalho@gmail.com>
2017-09-30 00:50:22 -0300

    adding initial authentication

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,5 +1,6 @@
-var {mongoose} = require('../db/db')
-var userSchema = mongoose.Schema({
+var {mongoose} = require('../db/db');
+
+var userSchema = new mongoose.Schema({
     text: {type: String, required: true},
     completed: {type: Boolean, default: false},
     completedAt: {type: Number, default: null}
--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-var {mongoose} = require('./db/db');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
-
+var _ = require('lodash');
+var {authenticate} = require('./middleware/authenticate');
 var app = express();
 var port = 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,resp)=> {
+app.post('/todos', authenticate, (req,resp)=> {
     console.log(`Inserting todo, text: ${req.body.text}`);
+    var body = _.pick(req.body,'text');
     var todo = Todo({
-        text: req.body.text
+        text: body.text
     });
 
     todo.save().then((doc) => {
@@ -37,9 +37,29 @@ app.get('/todos', (req,res) => {
     });
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
 app.listen(port , () => {
     console.log('Server started');
 })
+
+
 
 
 module.exports = {app};
